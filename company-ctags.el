9c2e099b3d4689d8aefea5389770fbb9f23e6030
Chen Bin <chenbin.sh@gmail.com>
2019-09-30 11:01:18 +1000

    minor issues

--- company-ctags.el
+++ company-ctags.el
@@ -1,14 +1,14 @@
-;;; company-ctags.el --- Fastest company-mode completion backend for ctags
+;;; company-ctags.el --- Fastest company-mode completion backend for ctags  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2019 Chen Bin
 
 ;; Author: Chen Bin <chenbin.sh@gmail.com>
-;; URL: http://github.com/redguardtoo/company-ctags
+;; URL: https://github.com/redguardtoo/company-ctags
 ;; Version: 0.0.1
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "24.3") (company "0.9.0"))
 
-;; This file is part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -21,8 +21,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -45,6 +44,9 @@
 ;;
 ;; You can also turn on `company-ctags-support-etags' to support tags
 ;; file created by etags.  But it will increase initial loading time.
+;;
+;; Make sure `diff-command' is executable on Windows. You might need install GNU Diff.
+;; It's used to speed up loading of updated tags file.
 
 ;;; Code:
 
@@ -354,7 +356,7 @@ Execute COMMAND with ARG and IGNORED."
 (defun company-ctags-replace-backend (backends)
   "Replace `company-etags' with `company-ctags' in BACKENDS."
   (let* (rlt)
-    (dolist (b company-backends)
+    (dolist (b backends)
       (cond
        ((eq b 'company-etags)
         (push 'company-ctags rlt))
