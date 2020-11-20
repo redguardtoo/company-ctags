b089a1e645cd14971c93397549b237ef63495adf
Chen Bin <chenbin.sh@gmail.com>
2020-11-20 17:05:07 +1100

    fixed unit test

--- test/company-ctags-tests.el
+++ test/company-ctags-tests.el
@@ -34,19 +34,6 @@
                             "\014\ntest.js,29\n"
                             "function hello() {\177hello\0011,0\n"))
 
-(defun tags-file-size ()
-  (let* ((rlt 0))
-    (dolist (s tags-file-content)
-      (setq rlt (+ (length s) rlt)))
-    rlt))
-
-(defun create-tags-file (filepath)
-  (with-temp-buffer
-    (apply #'insert tags-file-content)
-    ;; not empty
-    (should (> (length (buffer-string)) 50))
-    (write-region (point-min) (point-max) filepath)))
-
 (defun get-full-path (filename)
   (concat
    (if load-file-name (file-name-directory load-file-name) default-directory)
@@ -58,19 +45,16 @@
   (let* (cands
          (tags-file-name (get-full-path "TAGS"))
          file-info
-         file-size
+         (file-size (nth 7 (file-attributes tags-file-name)))
          dict)
-    (create-tags-file tags-file-name)
     (should (file-exists-p tags-file-name))
     (setq company-ctags-tags-file-caches nil)
     (should (company-ctags-load-tags-file tags-file-name nil t t t))
-    (should company-ctags-tags-file-caches)
-    (delete-file tags-file-name) ; clean up
     (setq file-info (gethash tags-file-name company-ctags-tags-file-caches))
     (should file-info)
     ;; check the file meta data
-    (should (eq (plist-get file-info :filesize) (tags-file-size)))
-    (should (eq (length (plist-get file-info :raw-content)) (tags-file-size)))
+    (should (eq (plist-get file-info :filesize) file-size))
+    (should (eq (length (plist-get file-info :raw-content)) file-size))
     (setq dict (plist-get file-info :tagname-dict))
     (should dict)))
 
@@ -80,73 +64,21 @@
          file-info
          dict
          cands)
-    (create-tags-file tags-file-name)
     (setq company-ctags-tags-file-caches nil)
     (should (company-ctags-load-tags-file tags-file-name nil t t t))
     (should company-ctags-tags-file-caches)
-    (delete-file tags-file-name) ; clean up
 
     (setq file-info (gethash tags-file-name company-ctags-tags-file-caches))
     (setq dict (plist-get file-info :tagname-dict))
     ;; completion with prefix
-    (setq cands (company-ctags-all-completions "C" dict))
+    (setq cands (company-ctags-all-candidates "C" dict))
     (should (eq (length cands) 1))
     (should (string= (car cands) "CHello"))
-    (setq cands (company-ctags-all-completions "he" dict))
-    (should (eq (length cands) 3))
-    (setq cands (company-ctags-all-completions "test" dict))
-    (should (eq (length cands) 1))
-    (setq cands (company-ctags-all-completions "h" dict))
-    (should (eq (length cands) 4))))
-
-(ert-deftest company-ctags-test-diff ()
-  (let* (cands
-         (tags-file-name (get-full-path "TAGS"))
-         file-info
-         dict
-         cands)
-
-    ;; load initial tags file
-    (create-tags-file tags-file-name)
-    (setq company-ctags-tags-file-caches nil)
-    (should (company-ctags-load-tags-file tags-file-name nil t t t))
-    (should company-ctags-tags-file-caches)
-    (delete-file tags-file-name) ; clean up
-
-    (setq file-info (gethash tags-file-name company-ctags-tags-file-caches))
-    (setq dict (plist-get file-info :tagname-dict))
-
-    ;; completion with prefix
-    (setq cands (company-ctags-all-completions "C" dict))
-    (should (eq (length cands) 1))
-    (should (string= (car cands) "CHello"))
-    (setq cands (company-ctags-all-completions "he" dict))
-    (should (eq (length cands) 3))
-    (setq cands (company-ctags-all-completions "test" dict))
-    (should (eq (length cands) 1))
-    (setq cands (company-ctags-all-completions "h" dict))
+    (setq cands (company-ctags-all-candidates "he" dict))
     (should (eq (length cands) 4))
-
-    ;; add more tags then load updated tags file using diff
-    (when (executable-find diff-command)
-      ;; diff is used when `company-ctags-tags-file-caches' is not empty
-      (should company-ctags-tags-file-caches)
-      ;; add new tags
-      (setq file-info (gethash tags-file-name company-ctags-tags-file-caches))
-      (setq dict (plist-get file-info :tagname-dict))
-      (push "function hello() {\177hello\0011,0\n" tags-file-content)
-      (push "function newtag() {\177newtag\0011,0\n" tags-file-content)
-      (create-tags-file tags-file-name)
-      (should (company-ctags-load-tags-file tags-file-name nil t nil t))
-      (delete-file tags-file-name) ; clean up
-
-      (setq file-info (gethash tags-file-name company-ctags-tags-file-caches))
-      (setq dict (plist-get file-info :tagname-dict))
-      (setq cands (company-ctags-all-completions "h" dict))
-      (should (eq (length cands) 5))
-      (setq cands (company-ctags-all-completions "he" dict))
-      (should (eq (length cands) 4))
-      (setq cands (company-ctags-all-completions "newtag" dict))
-      (should (eq (length cands) 1)))))
+    (setq cands (company-ctags-all-candidates "test" dict))
+    (should (eq (length cands) 1))
+    (setq cands (company-ctags-all-candidates "h" dict))
+    (should (eq (length cands) 5))))
 
 (ert-run-tests-batch-and-exit)
